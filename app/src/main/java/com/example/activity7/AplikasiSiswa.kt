037fc19dd854e8d190c33a9157c7e8d6f72a83a0
Reza-Riswandha <emrezariswanda@gmail.com>
2023-12-06 20:26:00 +0700

    Add class in AplikasiSiswa file

--- app/src/main/java/com/example/activity7/AplikasiSiswa.kt
+++ app/src/main/java/com/example/activity7/AplikasiSiswa.kt
@@ -1,4 +1,15 @@
 package com.example.activity7
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.activity7.repositori.ContainerApp
+import com.example.activity7.repositori.ContainerDataApp
+
+class AplikasiSiswa : Application() {
+
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
--- app/src/main/java/com/example/activity7/model/HomeViewModel.kt
+++ app/src/main/java/com/example/activity7/model/HomeViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.activity7.model
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.activity7.data.Siswa
 import com.example.activity7.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
 
@@ -15,4 +19,7 @@ class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
     data class HomeUiState(
         val listSiswa: List<Siswa> = listOf()
     )
+
+    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream().filterNotNull().map { HomeUiState(listSiswa = it.toList()) }.stateIn(scope = viewModelScope, started = SharingStarted.WhileSubscribed(
+        TIMEOUT_MILLIS), initialValue = HomeUiState())
 }
