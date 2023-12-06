8564a59f80b9610aa47a0337a0057953189729f6
Reza-Riswandha <emrezariswanda@gmail.com>
2023-12-06 20:18:11 +0700

    Add companion object in HomeViewModel

--- app/src/main/java/com/example/activity7/model/HomeViewModel.kt
+++ app/src/main/java/com/example/activity7/model/HomeViewModel.kt
@@ -5,5 +5,7 @@ import com.example.activity7.repositori.RepositoriSiswa
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
 
-
+    companion object{
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }
