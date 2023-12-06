3ed08fa79bc34c9523609c2c24809b8fdaf58190
Reza-Riswandha <emrezariswanda@gmail.com>
2023-12-06 20:17:23 +0700

    add class on HomeViewModel

--- app/src/main/java/com/example/activity7/model/HomeViewModel.kt
+++ app/src/main/java/com/example/activity7/model/HomeViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.activity7.model
 
+import androidx.lifecycle.ViewModel
+import com.example.activity7.repositori.RepositoriSiswa
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+
+
+}
