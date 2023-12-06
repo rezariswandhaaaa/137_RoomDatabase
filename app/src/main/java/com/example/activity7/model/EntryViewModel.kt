f483554d45c9062213ffc1e11c76139268babcf8
Reza-Riswandha <emrezariswanda@gmail.com>
2023-12-06 19:03:38 +0700

    add class in file EntryViewModel

--- app/src/main/java/com/example/activity7/model/EntryViewModel.kt
+++ app/src/main/java/com/example/activity7/model/EntryViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.activity7.model
 
+import androidx.lifecycle.ViewModel
+import com.example.activity7.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+
+
+}
