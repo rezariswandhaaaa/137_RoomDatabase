751511bd51db84156407aab62503e07699e7566d
Reza-Riswandha <emrezariswanda@gmail.com>
2023-12-06 20:20:58 +0700

    Add data class in HomeViewModel

--- app/src/main/java/com/example/activity7/model/HomeViewModel.kt
+++ app/src/main/java/com/example/activity7/model/HomeViewModel.kt
@@ -1,11 +1,18 @@
 package com.example.activity7.model
 
 import androidx.lifecycle.ViewModel
+import com.example.activity7.data.Siswa
 import com.example.activity7.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
 
     companion object{
         private const val TIMEOUT_MILLIS = 5_000L
     }
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
 }
