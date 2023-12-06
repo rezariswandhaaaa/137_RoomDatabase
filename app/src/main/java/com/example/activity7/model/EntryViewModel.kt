76a3c5c0ed0200b1b8db9466d656918fad7e57a9
Reza-Riswandha <emrezariswanda@gmail.com>
2023-12-06 19:14:15 +0700

    add DetailSiswa in EntryViewModel file

--- app/src/main/java/com/example/activity7/model/EntryViewModel.kt
+++ app/src/main/java/com/example/activity7/model/EntryViewModel.kt
@@ -1,9 +1,18 @@
 package com.example.activity7.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.activity7.repositori.RepositoriSiswa
 
 class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
 
-
 }
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
