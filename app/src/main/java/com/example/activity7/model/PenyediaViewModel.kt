41cc99362300872c49d74a480dbfc0fc572648b6
Reza-Riswandha <emrezariswanda@gmail.com>
2023-12-06 20:29:35 +0700

    Add object in PenyediaViewModel file

--- app/src/main/java/com/example/activity7/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/activity7/model/PenyediaViewModel.kt
@@ -1,2 +1,18 @@
 package com.example.activity7.model
 
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.activity7.AplikasiSiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(AplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(AplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
